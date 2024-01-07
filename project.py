def3f25039ec18e7403071565114abc9ef524605
nguyenkhainguyen0909 <153408917+nguyenkhainguyen0909@users.noreply.github.com>
2024-01-07 11:37:56 +0700

    á
    sa

--- project.py
+++ project.py
@@ -1,26 +1,52 @@
 from random import choice
 from turtle import *
-import turtle as player
+import arcade
+import turtle
 from freegames import floor, vector
 playerspeed = 50
-screen = player.Screen()
+sould = arcade.load_sound('D:\PHUC GIA in p1\python\SNLTW\hp2\project hp2\man-theme-remix-by-arsenic1987.mp3')
+arcade.play_sound(sould)
+pacman = turtle.Turtle()
+ghost1 = turtle.Turtle()
+ghost2 = turtle.Turtle()
+ghost3 = turtle.Turtle()
+ghost4 = turtle.Turtle()
+screen = turtle.Screen()
 screen.setup(600,600)
 screen.bgpic('D:\\PHUC GIA in p1\\python\\SNLTW\\hp2\\lesson7\\R.gif')
 screen.addshape('D:\\PHUC GIA in p1\\python\\SNLTW\\hp2\\lesson7\\pac-man.gif')
-player.shape('D:\\PHUC GIA in p1\\python\\SNLTW\\hp2\\lesson7\\pac-man.gif')
-#player.onkeypress(for)
-screen.update()
-screen.exitonclick()
+turtle.shape('D:\\PHUC GIA in p1\\python\\SNLTW\\hp2\\lesson7\\pac-man.gif')
+def eaten():
+    screen
 def moveLeft():
-    x = player.xcor()
+    x = turtle.xcor()
     x -= playerspeed
     if x < -200:
         x = -200
-    player.setx(x)
+    turtle.setx(x)
 def moveRight():
-    x = player.xcor()
+    x = turtle.xcor()
     x += playerspeed
-    if x > 200():
+    if x > 200:
         x =200
-    player.setx(x)
-moveLeft()
+    turtle.setx(x)
+def moveUp():
+    y = turtle.ycor()
+    y += playerspeed
+    if y > 200:
+        y =200
+    turtle.sety(y)
+def moveDown():
+    y = turtle.ycor()
+    y -= playerspeed
+    if y < -200:
+        y = -200
+    turtle.sety(y)
+screen.update()
+screen.onkeypress(moveLeft,"Left")
+screen.onkeypress(moveRight,"Right")
+screen.onkeypress(moveUp,"Up")
+screen.onkeypress(moveDown,"Down")
+screen.listen()
+screen.mainloop()
+screen.exitonclick()
